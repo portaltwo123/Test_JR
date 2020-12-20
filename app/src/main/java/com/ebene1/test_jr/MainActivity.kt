ae727cce2557ab9b47bdecabf88063e512504bfb
Jason Gloger <58487661+xSeeJay@users.noreply.github.com>
2020-12-20 12:26:23 +0100

    Update MainActivity.kt

--- app/src/main/java/com/ebene1/test_jr/MainActivity.kt
+++ app/src/main/java/com/ebene1/test_jr/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setupKategorien()
         //vefsefs
-
+        //ok 
 
     }
     private fun setupKategorien(){
