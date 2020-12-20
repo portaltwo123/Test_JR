02788f9696ba73a7ca860de0d62a4d6dc2d03f1c
portaltwo123 <73310614+portaltwo123@users.noreply.github.com>
2020-12-20 10:19:44 +0100

    Veränderung der TimerCategories

--- app/src/main/java/com/ebene1/test_jr/Timer_Categories.kt
+++ app/src/main/java/com/ebene1/test_jr/Timer_Categories.kt
@@ -1,5 +1,36 @@
 package com.ebene1.test_jr
 
 class TimerCategories(private var id:Int, private var name: String, private var image : Int, private var isSelected: Boolean) {
+    fun getId(): Int {
+        return id
+    }
+
+    fun setId(id: Int) {
+        this.id = id
+    }
+
+    fun getName(): String {
+        return name
+    }
+
+    fun setName(name: String) {
+        this.name = name
+    }
+
+    fun getImage(): Int {
+        return image
+    }
+
+    fun setImage(image: Int) {
+        this.image = image
+    }
+
+    fun getSelected(): Boolean {
+        return isSelected
+    }
+
+    fun setSelected(isSelected: Boolean) {
+        this.isSelected = isSelected
+    }
 
 }
